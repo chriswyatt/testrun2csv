603072ba9673f0cbdb983e6961b7f6b35a6631d4
Chris Wyatt <chris.wyatt@sgdigital.com>
2020-04-25 09:57:07 +0100

    Refactor

--- src/testrun2csv.nim
+++ src/testrun2csv.nim
@@ -1,219 +1,18 @@
 import algorithm
-import hashes
-import seqUtils
+import sequtils
 import streams
 import strformat
-import tables
-import xmlparser
-import xmltree
+import strutils
 
 import argparse
 
-type
-    Status = enum
-        error, failed, ignored, passed
-
-const statusSeq = [error, failed, ignored, passed]
-const statusStrSeq = ["error", "failed", "ignored", "passed"]
-const statusTableSize = statusSeq.len.rightSize
-
-const ordPeriod = ord('.')
-const ordZero = ord('0')
-const ordNine = ord('9')
-const ordAUpper = ord('A')
-const ordZUpper = ord('Z')
-const ordUnderscore = ord('_')
-const ordALower = ord('a')
-const ordZLower = ord('z')
+import parse_testrun
 
 const csvHeader = ["name", "status"]
 const csvHeaderStr = join(csvHeader, ",")
 
 type
     CsvRow = array[csvHeader.len, string]
-
-type
-    Suite = object
-        name*: string
-        status*: Status
-        locationUrl*: string
-
-type
-    Test = object
-        name*: string
-        status*: Status
-        locationUrl*: string
-        suites*: seq[Suite]
-
-proc hash(x: Suite): Hash =
-    var h: Hash = 0
-
-    h = h !& x.name.hash
-    h = h !& x.locationUrl.hash
-
-    result = !$h
-
-proc hash(x: Test): Hash =
-    var h: Hash = 0
-
-    for suite in x.suites:
-        h = h !& suite.hash
-
-    h = h !& x.name.hash
-    h = h !& x.locationUrl.hash
-
-    result = !$h
-
-proc getRoot(filePath: string): XmlNode =
-    let stream = newFileStream(filePath, fmRead)
-    result = parseXml(stream)
-    stream.close()
-
-proc getStatus(statusStr: string): Status =
-    result = statusSeq[find(statusStrSeq, statusStr)]
-
-proc parseTest(node: XmlNode): (string, Status, string) =
-    let name = node.attr("name")
-
-    let statusStr = node.attr("status")
-    let status = getStatus(statusStr)
-
-    let locationUrl = node.attr("locationUrl")
-
-    result = (name, status, locationUrl)
-
-proc parseSuite(node: XmlNode, parents = newSeq[Suite]()): seq[Test] =
-    let suiteName = node.attr("name")
-
-    let suiteStatusStr = node.attr("status")
-    let suiteStatus = getStatus(suiteStatusStr)
-
-    let suiteLocationUrl = node.attr("locationUrl")
-
-    let suite = Suite(
-        name: suiteName,
-        status: suiteStatus,
-        locationUrl: suiteLocationUrl,
-    )
-
-    result = newSeq[Test]()
-
-    for child in node:
-        if child.tag == "test":
-            let (testName,
-                 testStatus,
-                 testLocationUrl) = parseTest(child)
-
-            result.add(Test(
-                name: testName,
-                status: testStatus,
-                locationUrl: testLocationUrl,
-                suites: parents & @[suite],
-            ))
-        elif child.tag == "suite":
-            for test in parseSuite(child, parents=parents & @[suite]):
-                result.add(test)
-
-proc parseRoot(root: XmlNode): (
-        int,
-        CountTableRef[Status],
-        seq[Test],
-) =
-    var testSeq = newSeq[Test]()
-
-    var numTests: int
-    let statusCounts = newCountTable[Status](
-        initialSize = statusTableSize,
-    )
-
-    for child in root:
-        if child.tag == "suite":
-            for test in parseSuite(child):
-                testSeq.add(test)
-        elif child.tag == "count":
-            let countName = child.attr("name")
-            let countValueStr = child.attr("value")
-            if countName == "total":
-                numTests = parseInt(countValueStr)
-            else:
-                let status = getStatus(countName)
-
-                if statusCounts.hasKey(status):
-                    raise newException(
-                        ValueError,
-                        "Duplicate 'count' elements",
-                    )
-
-                statusCounts[status] = parseInt(countValueStr)
-
-    return (numTests, statusCounts, testSeq)
-
-proc isNameValid(name: string): bool =
-    for chk in name:
-        let ordChk = ord(chk)
-
-        if not (
-                (ordChk >= ordALower and ordChk <= ordZLower) or
-                (ordChk >= ordZero and ordChk <= ordNine) or
-                (ordChk >= ordAUpper and ordChk <= ordZUpper) or
-                ordChk == ordUnderscore or
-                ordChk == ordPeriod):
-
-            return false
-    
-    return true
-
-proc validate(
-        expectedNumTests: int,
-        expectedStatusCounts: CountTableRef[Status],
-        testSeq: seq[Test],
-) =
-    let actualStatusCounts = newCountTable[Status](
-        initialSize = statusTableSize,
-    )
-
-    for test in testSeq:
-        actualStatusCounts.inc(test.status)
-
-    if actualStatusCounts != expectedStatusCounts:
-        echo $actualStatusCounts
-        echo $expectedStatusCounts
-        raise newException(ValueError, "Unexpected status counts")
-
-    var actualNumTests: int
-    for count in values(actualStatusCounts):
-        actualNumTests += count
-
-    if actualNumTests != expectedNumTests:
-        raise newException(ValueError, "Unexpected total")
-
-    let testHashCounts = newCountTable[Hash](
-        initialSize = actualNumTests.rightSize,
-    )
-    for test in testSeq:
-        testHashCounts.inc(test.hash)
-
-    let largestTestHashCount = testHashCounts.largest
-    if largestTestHashCount[1] > 1:
-        raise newException(ValueError, "Duplicate test(s)")
-
-    for test in testSeq:
-        for suite in test.suites:
-            if not isNameValid(suite.name):
-                raise newException(ValueError, "Suite name invalid")
-
-        if not isNameValid(test.name):
-            raise newException(ValueError, "Test name invalid")
-
-proc parseFile(inFilePath: string): (
-        int,
-        CountTableRef[Status],
-        seq[Test],
-) =
-    let root = getRoot(inFilePath)
-    let (numTests, statusCounts, testSeq) = parseRoot(root)
-    validate(numTests, statusCounts, testSeq)
-    return (numTests, statusCounts, testSeq)
 
 proc testCmp(x, y: Test): int =
     let minSuitesLen = min(x.suites.len, y.suites.len)
@@ -255,7 +54,7 @@ proc writeCsv(testSeq: seq[Test]) =
 
         echo join(row, ",")
 
-proc parseArgs(): string =
+proc parseArgs: string =
     let p = newParser("testrun2csv"):
         help("Convert IntelliJ testrun XML file to a CSV file")
         arg(
@@ -277,5 +76,5 @@ try:
 except UsageError:
     quit(QuitFailure)
 
-let (numTests, statusCounts, testSeq) = parseFile(inFilePath)
+let testSeq = parseFile(inFilePath)
 writeCsv(testSeq)
